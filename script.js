2e1cad9248c08db756b43e14a95c2f3152284bd4
frogshark7 <31362256+frogshark7@users.noreply.github.com>
2018-12-30 14:41:07 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+(window.onload = function() {
+	var elem = document.getElementById('hplogo');
+    elem.parentNode.removeChild(elem);
+
+	var img = document.createElement("img");
+	img.src = "https://media.giphy.com/media/oNwtfA16trRv0nSCez/giphy.gif";
+	img.setAttribute("id", "custom-g");
+	
+	var img2 = document.createElement("img");
+	img2.src = "https://media.giphy.com/media/lzCY0C6M50jbe29R1B/giphy.gif";
+	img2.setAttribute("id", "custom-load");
+
+	var img3 = document.createElement("img");
+	img3.src = "https://i.imgur.com/0YDjzkk.png";
+	img3.setAttribute("id", "custom-last");
+
+	var parent = document.getElementById("lga");
+	parent.appendChild(img);
+	parent.appendChild(img2);
+	parent.appendChild(img3);
+})();
+
